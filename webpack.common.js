2aba4a27cf0307043e1b67f1a505edbdc17f2ed7
Chris Wong <wh18710455626@126.com>
2021-12-08 12:46:39 +0800

    Add prod and dev config, use webpack-dev-server

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,7 @@
-const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    // devTool: 'none',  // no eval
     entry: './src/index.js',
-    output: {
-        filename: 'main.[contenthash].js',
-        path: path.resolve(__dirname, "dist")
-    },
     module: {  // under module.rules => loaders basic
         rules: [
             {
